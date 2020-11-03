a688c26bfd25aafbc73aaac9d10c9922df60cbc1
Nam <sjarbgud@gmail.com>
2020-11-03 22:06:54 +0900

    @Componentscan - 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
+// @ComponentScan 디폴트는 해당 클래스가 있는 위치의 패키지를 다 스캔한다.
+// 권장방법 -> 프로젝트의 최상단에 두어서 모든 컴포넌트들을 스캔
+/* 컴포넌트 스캔 기본 대상
+    @Component: 컴포넌트 스캔에서 사용
+    @Controller: 스프링 MVC 컨트롤러에서 사용
+    @Service: 스프링 비즈니스 로직에 사용
+    @Repository: 스프링 데이터 접근 계층 사용
+    @Configuration: 스프링 설정 정보 사용
+ */
 @ComponentScan(
+        // basePackages: 탐색할 패키지의 시작 위치를 지정한다.
+        basePackages = "hello.core.member",
         // AppConfig도 @Component이기 때문에 제외한다 (@Configuration이 붙은것 제외)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
