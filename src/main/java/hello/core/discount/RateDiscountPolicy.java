2839c2bb0d50b973c056c9d43b5c3ed5ae5a7a4c
Nam <sjarbgud@gmail.com>
2020-11-18 23:41:34 +0900

    애노테이션 직접 만들기

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,11 +1,15 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscountPolicy") --> 문자라 컴파일시 타입 체크가 안된다.
+@MainDiscountPolicy     //@Primary 사용로도 가능하지만 이렇게 애노테이션 생성후 사용가능
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
